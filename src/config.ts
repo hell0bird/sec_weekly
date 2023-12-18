d59585670e0b5b7104767232d439c35788e8d00b
Hellobird <jbaodong@gmail.com>
2023-12-18 14:47:24 +0800

    更新

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
 export const SITE = {
-  title: '潮流周刊',
-  author: 'Tw93',
-  description: '记录工程师 Tw93 的不枯燥生活，每周一发布，欢迎关注',
-  keywords: 'Tw93,Weekly,Blog,前端,macOS,MiaoYan,Pack,Swift',
+  title: '网安周刊',
+  author: 'hellobird',
+  description: '雁过留痕,风过留声',
+  keywords: 'hellobird,网安,tsec',
   icon: 'https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png',
   pic: 'https://cdn.fliggy.com/upic/2XFNGI.png',
-  homePage: 'https://weekly.tw93.fun',
-  blogPage: 'https://tw93.fun/',
-  twitterId: 'HiTw93',
-  githubId: 'tw93',
-  repo: 'tw93/weekly'
+  homePage: 'https://weekly.tsec.fun',
+  blogPage: 'https://tsec.fun/',
+  twitterId: '',
+  githubId: 'hell0bird',
+  repo: 'hell0bird/sec_weekly'
 };
 
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -16,7 +16,7 @@ export const get = () =>
   rss({
     title: '潮流周刊',
     description: '记录 Tw93 潮流前端的日常生活',
-    site: 'https://weekly.tw93.fun/',
+    site: 'https://weekly.tsec.fun/',
     customData: `<image><url>https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png</url></image>`,
     items: posts.map((item) => {
       const url = item.url;

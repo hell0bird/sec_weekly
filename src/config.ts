49e5ba8c92b1fe0d58d930f9a939c2bcf54fcaa9
Hellobird <jbaodong@gmail.com>
2023-12-18 15:36:01 +0800

    更新

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const SITE = {
   title: '网安周刊',
   author: 'hellobird',
   description: '雁过留痕,风过留声',
-  keywords: 'hellobird,网安,tsec',
+  keywords: 'hellobird,网安,tsec,网络安全,信息安全',
   icon: 'https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png',
   pic: 'https://cdn.fliggy.com/upic/2XFNGI.png',
   homePage: 'https://weekly.tsec.fun',

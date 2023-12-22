be5b63b76a89b567c3bf3fac911c49ad379c12f6
Hellobird <jbaodong@gmail.com>
2023-12-22 13:14:28 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-hellobird
+# 网络安全笔记
+
+> 给枯燥的生活添加点乐趣，欢迎订阅，也欢迎 [推荐](https://github.com/hell0bird/sec_weekly/discussions/1) 你的好东西。
--- build.py
+++ build.py
@@ -12,7 +12,7 @@ def fetch_ci_time(filePath):
 
 if __name__ == "__main__":
   readmefile=open('README.md','w')
-  readmefile.write("# 网络安全笔记\n\n> 给枯燥的生活添加点乐趣，欢迎订阅，也欢迎 [推荐](https://github.com/hell0bird/sec_weekly/discussions/1) 你的好东西。
+  readmefile.write("# 网络安全笔记\n\n> 给枯燥的生活添加点乐趣，欢迎订阅，也欢迎 [推荐](https://github.com/hell0bird/sec_weekly/discussions/1) 你的好东西。")
   recentfile=open('RECENT.md','w')
 
   for root, dirs, filenames in os.walk('./src/pages/posts'):
@@ -25,14 +25,8 @@ if __name__ == "__main__":
         url   = 'https://weekly.tsec.fun/posts/' + oldTitle
         title = '第 ' + oldTitle.split('-')[0] + ' 期 - ' + oldTitle.split('-')[1]
         readmeMd= '* [{}]({})\n'.format(title, url)
-        dateList = ["2022-10-10","2022-09-26","2022-09-12","2022-09-05","2022-08-29"]
-        num = int(oldTitle.split('-')[0])
         if index < 5 :
-          if num < 100 :
-            modified = dateList[99-num]
-          else :
-            modified = fetch_ci_time('/src/pages/posts/' + filepath)
-
+          modified = fetch_ci_time('/src/pages/posts/' + filepath)
           recentMd= '* [{}]({}) - {}\n'.format(title, url, modified)
           recentfile.write(recentMd)
         readmefile.write(readmeMd)
